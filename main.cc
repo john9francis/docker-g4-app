d7defb6637fbb5a493fc4a99f0c4d5183366d7c2
John Francis <john9francis@gmail.com>
2024-09-17 03:09:37 +0000

    Added a bit of scaffolding for our Geant4 app

--- main.cc
+++ main.cc
@@ -1,9 +1,25 @@
 // Entrypoint for docker-g4-app
 
-#include "G4VUserActionInitialization.hh"
-#include "G4VUserDetectorConstruction.hh"
+#include <iostream>
+#include "G4RunManagerFactory.hh"
 
 int main(){
+
+  // Todo:
+
+  // Get the g4 app compiling in the first place...
+
+  // run manager
+  auto runManager = G4RunManagerFactory::CreateRunManager();
+
+  std::cout << "Hello from the Geant4 world!" << std::endl;
+  
+  // action initialization
+  // physics list
+  // primary generator action
+  // detector construction
+
+  // stepping action
 
   return 0;
 }
